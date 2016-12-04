9ae2fa1eb5a6c514207d1ada1dae5d0e4b069910
Corp <corpa@wit.edu>
2016-12-04 18:51:41 -0500

    Board now prints from Array

--- finalProject.asm
+++ finalProject.asm
@@ -10,15 +10,9 @@ INCLUDELIB \masm32\lib\Irvine32.lib
 ExitProcess PROTO, DwErrorCode:DWORD
 
 .DATA
-	Tile STRUCT
-		X WORD ?
-		Y WORD ?
-		bgColor Byte ?; background color of the tile
-		piece Byte ' '; if no piece then its nothing, else its a letter
-	Tile ENDS
 	
-	tiles Tile 64 DUP (<>)
-
+	tiles Byte 64 DUP ('K'); this is the 'piece' that is on the tile
+                             ; defaults to have a space on it.
 	
 .CODE
 
@@ -50,16 +44,16 @@ ExitProcess PROTO, DwErrorCode:DWORD
 
   DrawBoard PROC
   
-    mov bl, 65; ascii val of char to write
-    mov ecx, 8; loop count
+    mov bl, 65 ; ascii val of char to write
+    mov ecx, 8 ; loop count
 
     mov al, ' '
     Call WriteChar
     
     DrawLettersLoop:
 
-        mov al, bl; put the char into al example:('A')
-        Call WriteChar; write the letter
+        mov al, bl ; put the char into al example:('A')
+        Call WriteChar ; write the letter
         inc bl
         
         dec ecx
@@ -68,48 +62,68 @@ ExitProcess PROTO, DwErrorCode:DWORD
         
     Call Crlf
 
-    mov eax, 56; current line number to write
-    mov ecx, 8
-
-    ;mov esi, 1; column index
-    mov ebx, 1
+    mov eax, 56 ; current line number to write
+    mov ecx, 0 ; row index
+    mov ebx, 1 ; counter for tile pattern
     
     DrawCoordsLoop:
-	   mov esi, 0
-	   push ecx
-	   push eax
-         Invoke SetTextColor, lightGray, black
-		 pop eax
-         Call WriteChar
-		push eax
-        InnerLoop:
-		Invoke GetColor, ebx
+	   mov esi, 0 ; column index
+	   push ecx ; preserve ecx value (SetTextColor changes it)
+	   push eax ; preserve eax value (SetTextColor changes it)
 
-            push ebx
-    
+         Invoke SetTextColor, lightGray, black ; set the number text to be the correct color
+         
+         pop eax ; restore value
+         pop ecx ; restore value
+         
+         Call WriteChar ; write the row number
+         push eax ; preserve eax value to use for next iteration
+
+         InnerLoop:
+		Invoke GetColor, ebx ; alternates white and black
+
+            push ebx ; preserve ebx
+            
             mov ebx, 15
-            sub ebx, eax
-		Invoke SetTextColor, ebx, eax
-            pop ebx
-		mov eax, ' '
+            push ecx ; preserve ecx value (SetTextColor changes it)
+            sub ebx, eax ; inverse color of text to background
+		Invoke SetTextColor, ebx, eax ; sets the background color
+            
+            ; This block gets the character to write from the array
+            ; then writes the char to console.
+            mov eax, 0
+            mov ebx, 0
+
+            ;Does the math to get the correct index
+            ; X + (width * Y)
+            pop ecx
+            mov eax, 8
+            mul ecx
+            add eax, esi
+
+            mov ebx, OFFSET tiles
+            add ebx, eax
+            mov eax, [ebx]
+            
 		Call WriteChar
-			
-            inc esi
-            inc ebx
+
+		pop ebx ; restore ebx
+
+            inc esi ; move to next tile
+            inc ebx ; increment overall counter
             cmp esi, 8
-            jne InnerLoop
+            jne InnerLoop ; loop if not at end of row
         
-        Call Crlf; tentative
+        Call Crlf
         
 	  pop eax
-	  pop ecx
        
         dec eax
         inc ebx
-        dec ecx
-        cmp ecx, 0
-        jne DrawCoordsLoop
-		
+        inc ecx
+        cmp ecx, 8
+        jne DrawCoordsLoop ; loop if not at end of board
+        
     ret
     
   DrawBoard ENDP
